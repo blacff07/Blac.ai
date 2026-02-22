e77992a215e297e686948a49ab08cbc9d9007188
blacff07 <blacfflite@gmail.com>
2026-02-22 20:49:03 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,12 +5,13 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") } // For Prism4j
+        maven { url = uri("https://jitpack.io") } // Required for Prism4j and other libraries
     }
 }
 
