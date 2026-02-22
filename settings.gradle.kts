e4ed955683afe9bb1f77e5cbf8701439da3854bd
blacff07 <blacfflite@gmail.com>
2026-02-22 23:37:36 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        // Correct JitPack configuration for Prism4j
         maven {
             url = uri("https://jitpack.io")
         }
