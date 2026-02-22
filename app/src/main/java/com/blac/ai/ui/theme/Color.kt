814bd79eafe5bb1028e90db299bac9a153c4c3b1
blacff07 <blacfflite@gmail.com>
2026-02-22 11:00:46 +0530

    Delete Color.kt

--- app/src/main/java/com/blac/ai/ui/theme/Color.kt
+++
@@ -1,7 +0,0 @@
-package com.blac.ai.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val primaryDark = Color(0xFF1E1E2F)
-val secondaryDark = Color(0xFF2D2D3A)
-val accent = Color(0xFF7C4DFF)
