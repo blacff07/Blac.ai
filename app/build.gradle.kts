37c62b185f97fbba11610cfb0e5fdd7b2404c607
blacff07 <blacfflite@gmail.com>
2026-02-22 21:20:26 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,7 +89,6 @@ dependencies {
     // Replace the current prism4j line with:
 implementation("com.github.noties:prism4j:2.0.0")
 implementation("com.github.noties:prism4j-bundler:2.0.0")
-    implementation("com.github.noties:prism4j-bundler:2.0.0")
 
     // OkHttp for networking
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
